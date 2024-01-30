ead2b677fb6103f64aee63cc7e5e2bcc299854a4
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2024-01-30 14:40:13 -0600

    updating def get_totalPoints

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ def display_title():
     print("Welcome to the Grade Calculator")
     print("")
 
-# Get total points
+
+# Get total points from input and repeat if input does not meet expected values.
+def get_totalPoints():
+    totalPoints = 0.0
+    while (totalPoints == 0):
+        try:
+            totalPoints = float(input("Enter the total score (0-1000): "))
+            if totalPoints < 0 or totalPoints > 1000:
+                print("You must enter integer values >=0 and <= 1000. Please try again.")
+            else:
+                return totalPoints
+        except ValueError:
+            print("You must enter integer values >=0 and <= 1000. Please try again.")
