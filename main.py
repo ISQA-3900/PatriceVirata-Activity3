126ac5705a48d4bcb575fd3b73e93c30c89afcaf
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2024-01-30 13:53:53 -0600

    Getting started with Pycharm files

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Name: Patrice Virata")
+print("ISQA3900-001 January 30th, 2024")
+print("This program evaluates the letter grade for a class in a scale of 1000 points.")
+
+# Displays the title of the program
+def display_title():
+    print("Welcome to the Grade Calculator")
+    print("")
+
+# Get total points
