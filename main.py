cd77bccd057cc902e6760447b48abad7f63266a1
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2024-01-30 13:56:25 -0600

    Pycharm update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("Name: Patrice Virata")
 print("ISQA3900-001 January 30th, 2024")
 print("This program evaluates the letter grade for a class in a scale of 1000 points.")
 
+
 # Displays the title of the program
 def display_title():
     print("Welcome to the Grade Calculator")
