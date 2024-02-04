83713647c321e4818073b8c0ac5ed7507d9076d3
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2024-02-03 22:31:37 -0600

    debug main.py

--- main.py
+++ main.py
@@ -12,28 +12,35 @@ def display_title():
 # Get total points from input and repeat if input does not meet expected values.
 def get_totalPoints():
     totalPoints = 0.0
-    while 0 < totalPoints < 1000:
-        totalPoints = int(input("Enter the total score (0-1000): "))
-        if totalPoints < 0 or totalPoints > 1000:
-            print("You must enter integer values >=0 and <= 1000. Please try again.")
-        else:
-            return totalPoints
+    averageEarned = totalPoints
+    while totalPoints == 0.0:
+        try:
+            totalPoints = int(input("Enter the total score (0-1000): "))
+            if totalPoints < 0 or totalPoints > 1000:
+                print("You must enter integer values >= 0 and <= 1000. Please try again.")
+                totalPoints = 0.0
+                print("")
+            else:
+                return totalPoints
+        except ValueError:
+            print("You must enter integer values >= 0 and <= 1000. Please try again.")
+            print("")
 
 
 # Convert points into a letter grade
 def get_letterGrade(averageEarned):
-    averageEarned = (averageEarned / 10.0)
+    averageEarned = averageEarned/10
     if 92.0 <= averageEarned <= 1000:
         letterGrade = "A"
-    elif 88 <= averageEarned <= 91.99:
+    elif 88.0 <= averageEarned <= 91.99:
         letterGrade = "B+"
-    elif 82 <= averageEarned <= 87.99:
+    elif 82.0 <= averageEarned <= 87.99:
         letterGrade = "B"
-    elif 78 <= averageEarned <= 81.99:
+    elif 78.0 <= averageEarned <= 81.99:
         letterGrade = "C+"
-    elif 70 <= averageEarned <= 77.99:
+    elif 70.0 <= averageEarned <= 77.99:
         letterGrade = "C"
-    elif 60 <= averageEarned <= 69.99:
+    elif 60.0 <= averageEarned <= 69.99:
         letterGrade = "D"
     else:
         letterGrade = "F"
@@ -46,6 +53,15 @@ def main():
     display_title()
     averageEarned = get_totalPoints()
     get_letterGrade(averageEarned)
-
+    print("")
+    repeatAgain = str(input("Would you like to enter another score (y/n)? ").lower())
+    print("")
+    while repeatAgain == "y":
+        averageEarned = get_totalPoints()
+        get_letterGrade(averageEarned)
+        print("")
+        repeatAgain = str(input("Would you like to enter another score (y/n)? ").lower())
+        print("")
+    print("Thank you")
 
 main()
